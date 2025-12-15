deda5e42768bd097a23ee6ba725f10b52ad29b60
Dominik Hladík <dominik.hladik@seznam.cz>
2025-12-16 00:21:36 +0100

    feat(transloco): transloco added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideTransloco } from '@jsverse/transloco';
 import { routes } from './app.routes';
+import { TranslocoHttpLoader } from './transloco-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withViewTransitions(), withComponentInputBinding()),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'cs'],
+        defaultLang: 'en',
+        // Remove this option if your application doesn't support changing language in runtime.
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
   ],
 };
