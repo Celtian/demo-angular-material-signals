b6c9fd476af38004c434a969ba9cd7d51e84d736
Dominik Hladík <dominik.hladik@seznam.cz>
2025-12-16 08:30:46 +0100

    feat(forms): form added

--- src/app/pages/post/post-edit/post-edit.ts
+++ src/app/pages/post/post-edit/post-edit.ts
@@ -1,12 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { Field, form, required } from '@angular/forms/signals';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-post-edit',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, Field, TranslocoPipe, JsonPipe],
   templateUrl: './post-edit.html',
   styleUrl: './post-edit.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: 'block container m-auto py-4 space-y-2' },
 })
-export class PostEdit {}
+export class PostEdit {
+  public readonly model = signal({ title: '', content: '' });
+  public readonly form = form(this.model, (path) => [required(path.title), required(path.content)]);
+
+  public onSubmit(event: Event): void {
+    event.preventDefault();
+    console.log(this.form().value());
+  }
+}
