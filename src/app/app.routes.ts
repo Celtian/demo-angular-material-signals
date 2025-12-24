5f2e640cff1aee860dbcbad15bcaab6c380042cf
Dominik Hladík <dominik.hladik@seznam.cz>
2025-12-24 21:28:05 +0100

    feat(routes): seo added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
+import { inject } from '@angular/core';
 import { Routes } from '@angular/router';
+import { TranslocoService } from '@jsverse/transloco';
 import { marker as _ } from '@jsverse/transloco-keys-manager/marker';
 import { ROUTE_DEFINITION } from './constant/route-definition.constant';
 import { CanDeactivateGuardService } from './guards/can-deactivate-guard.service';
@@ -7,6 +9,7 @@ export const BREADCRUMBS = {
   LIST: _('breadcrumb.list'),
   CREATE: _('breadcrumb.create'),
   EDIT: _('breadcrumb.edit'),
+  DETAIL: _('breadcrumb.detail'),
   NOT_FOUND: _('breadcrumb.not-found'),
 };
 
@@ -14,6 +17,10 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/post/post-list/post-list').then((m) => m.PostList),
+    title: () => {
+      const transloco = inject(TranslocoService);
+      return transloco.translate(BREADCRUMBS.LIST);
+    },
     data: {
       breadcrumb: {
         label: BREADCRUMBS.LIST,
@@ -26,6 +33,10 @@ export const routes: Routes = [
   {
     path: ROUTE_DEFINITION.POSTS.CREATE,
     loadComponent: () => import('./pages/post/post-create/post-create').then((m) => m.PostCreate),
+    title: () => {
+      const transloco = inject(TranslocoService);
+      return transloco.translate(BREADCRUMBS.CREATE);
+    },
     data: {
       breadcrumb: {
         label: BREADCRUMBS.CREATE,
@@ -42,11 +53,19 @@ export const routes: Routes = [
       {
         path: '',
         loadComponent: () => import('./pages/post/post-detail/post-detail').then((m) => m.PostDetail),
+        title: () => {
+          const transloco = inject(TranslocoService);
+          return transloco.translate(BREADCRUMBS.DETAIL);
+        },
       },
       {
         path: ROUTE_DEFINITION.POSTS.EDIT,
         loadComponent: () => import('./pages/post/post-edit/post-edit').then((m) => m.PostEdit),
         canDeactivate: [CanDeactivateGuardService],
+        title: () => {
+          const transloco = inject(TranslocoService);
+          return transloco.translate(BREADCRUMBS.EDIT);
+        },
         data: {
           breadcrumb: {
             label: BREADCRUMBS.EDIT,
@@ -61,6 +80,10 @@ export const routes: Routes = [
   {
     path: '**',
     loadComponent: () => import('./pages/not-found/not-found').then((m) => m.NotFound),
+    title: () => {
+      const transloco = inject(TranslocoService);
+      return transloco.translate(BREADCRUMBS.NOT_FOUND);
+    },
     data: {
       breadcrumb: {
         label: BREADCRUMBS.NOT_FOUND,
