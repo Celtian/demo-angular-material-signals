8d1ca438f5f2e4bbf61185e6eb80f11ccb17fce0
Dominik Hladík <dominik.hladik@seznam.cz>
2025-12-24 22:21:40 +0100

    chore(translations): missing base href added

--- src/app/transloco-loader.ts
+++ src/app/transloco-loader.ts
@@ -1,3 +1,4 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Translation, TranslocoLoader } from '@jsverse/transloco';
@@ -5,8 +6,9 @@ import { Translation, TranslocoLoader } from '@jsverse/transloco';
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
   private readonly http = inject(HttpClient);
+  private readonly baseHref = inject(APP_BASE_HREF, { optional: true }) ?? '';
 
   public getTranslation(lang: string) {
-    return this.http.get<Translation>(`/i18n/${lang}.json`);
+    return this.http.get<Translation>(`${this.baseHref}i18n/${lang}.json`);
   }
 }
